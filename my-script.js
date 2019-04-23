d90a471dbcb7b9f9835e72f11933a24c23dc1472
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2019-04-23 21:16:52 +0200

    Final minor changes

--- my-script.js
+++ my-script.js
@@ -1,4 +1,9 @@
+// taking the root div from the index.html
+// to use further here in the script and declare it as variable "app"
 const app = document.getElementById('root')
+
+// declaring all the variables and divs so I can use them later
+// and structure the hierarchy between parents and children
 
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
@@ -11,11 +16,11 @@ carousel.setAttribute('class', 'carousel')
 const buttonWrapper = document.createElement('div')
 buttonWrapper.setAttribute('class', 'button-wrapper')
 
-const slideLeft = document.createElement ('img')
+const slideLeft = document.createElement('img')
 slideLeft.setAttribute('class', 'slide-left')
 slideLeft.src = "https://img.icons8.com/ios-glyphs/25/000000/chevron-left.png"
 
-const slideRight = document.createElement ('img')
+const slideRight = document.createElement('img')
 slideRight.setAttribute('class', 'slide-right')
 slideRight.src = "https://img.icons8.com/ios-glyphs/25/000000/chevron-right.png"
 
@@ -24,12 +29,16 @@ container.appendChild(buttonWrapper)
 buttonWrapper.appendChild(slideLeft)
 buttonWrapper.appendChild(slideRight)
 
-fetch("https://randomuser.me/api/?results=30")
-.then((response) => {
-  return response.json()
-}).then((data) => {
-  const results = data.results
+// fetching the data from the API, decided to only fetch 30 results
 
+fetch("https://randomuser.me/api/?results=30")
+  .then((response) => {
+    return response.json()
+  }).then((data) => {
+    // saving the data in a variable so I can easily see what I get and choose what to use
+    const results = data.results
+
+    // I use the data to create divs (in this case a class called "card") for each result I get (in this case 30)
     results.forEach((item) => {
       const card = document.createElement('div')
       card.setAttribute('class', 'card')
@@ -43,31 +52,39 @@ fetch("https://randomuser.me/api/?results=30")
       const p = document.createElement('p')
       p.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
 
+      // to each card I append the children: img, h1 and p
+      // which are declared above
       carousel.appendChild(card)
       card.appendChild(img)
       card.appendChild(h1)
       card.appendChild(p)
 
-      const carouselWidth = carousel.offsetWidth;
+      // define the width of the carousel so I know how much the cards need to slide
+      const carouselWidth = carousel.offsetWidth
       const cardStyle = card.currentStyle || window.getComputedStyle(card)
-      const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
-      const cardCount = 30;
+      const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0])
+      const cardCount = 30
+
+      // define when the buttons are able to clicked depending
+      // if there is more to show in either direction
       let offset = 0;
       const maxX = -((cardCount / 3) * carouselWidth +
-                    (cardMarginRight * (cardCount / 3)) -
-                    carouselWidth - cardMarginRight);
+        (cardMarginRight * (cardCount / 3)) -
+        carouselWidth - cardMarginRight);
+
+      // the click events that only works if there are cards to show left or right
       slideLeft.addEventListener("click", function() {
         if (offset !== 0) {
           offset += carouselWidth + cardMarginRight;
-            carousel.style.transform = `translateX(${offset}px)`;
-              }
-            })
+          carousel.style.transform = `translateX(${offset}px)`;
+        }
+      })
 
       slideRight.addEventListener("click", function() {
         if (offset !== maxX) {
           offset -= carouselWidth + cardMarginRight;
-            carousel.style.transform = `translateX(${offset}px)`;
-                }
-            })
+          carousel.style.transform = `translateX(${offset}px)`;
+        }
+      })
     })
   })
